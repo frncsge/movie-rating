1edebd360024af2904d60e0bbc176a75163d117c
Francis Ge <francisge527@gmail.com>
2025-08-21 18:49:52 +0800

    configure env

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  password: "goofygoober",
-  host: "localhost",
-  port: 5432,
-  database: "movie_rating_db",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 db.connect();
 
